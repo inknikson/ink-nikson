8d6db4812b730197aebbe4114b4613cf1a0f8a30
inknikson <inknikson@gmail.com>
2026-06-03 09:05:28 +0200

    premier commit ink-nikson

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function Home() {
+  const html = fs.readFileSync(
+    path.join(process.cwd(), 'public', 'ink-nikson.html'),
+    'utf-8'
+  );
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
+}
